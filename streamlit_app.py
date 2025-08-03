d153f99f64914fedc5d8c4a8d1b4d500889ad399
Devansh <devansh.s.sachan@kipi.ai>
2025-08-03 22:32:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,29 @@
 import requests
 import pandas as pd
 import streamlit as st
-import requests
 from snowflake.snowpark.functions import col
 
 # App title
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
+# Input: Name on order
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
+
+# Input: Mark order as filled
+is_filled = st.checkbox("Mark order as FILLED")
 
 # Snowflake session setup
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-# Get fruit list from Snowflake
-# Get fruit name and search term
+# Get fruit name and search term from Snowflake
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
 pd_df = my_dataframe.to_pandas()
 fruit_names = pd_df["FRUIT_NAME"].tolist()
 
-
-# Multiselect input
+# Multiselect input for ingredients
 ingredients_list = st.multiselect("Choose up to 5 ingredients:", fruit_names, max_selections=5)
 
 # Display nutrition info if fruits are selected
@@ -34,24 +35,21 @@ if ingredients_list:
 
         st.subheader(fruit_chosen + ' Nutrition Information')
 
-        # Call SmoothieFroot API using fruit name
         # Lookup SEARCH_ON value from pd_df
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
+        response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
 
-        # If found, display in dataframe. If not, show fallback message
-        if smoothiefroot_response.status_code == 200:
-            sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        if response.status_code == 200:
+            st.dataframe(data=response.json(), use_container_width=True)
         else:
             st.error(f"Sorry, {fruit_chosen} is not in our database.")
-
-    # Display nutrition info in a DataFrame
-    if all_nutrition_data:
-        st.dataframe(data=all_nutrition_data, use_container_width=True)
 
     # Insert into database
     time_to_insert = st.button("Submit Order")
     if time_to_insert:
-        insert_stmt = f"INSERT INTO smoothies.public.orders(ingredients) VALUES ('{ingredients_string}')"
+        insert_stmt = f"""
+            INSERT INTO smoothies.public.orders(name_on_order, ingredients, filled)
+            VALUES ('{name_on_order}', '{ingredients_string.strip()}', {is_filled})
+        """
         session.sql(insert_stmt).collect()
         st.success("Your Smoothie is ordered!", icon="✅")
